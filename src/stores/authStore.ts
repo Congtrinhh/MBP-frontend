3d69289bd67ea7d0b7ff7f0b4948a5a024a5025c
Congtrinhh <trinhquycong@gmail.com>
2025-01-16 19:31:51 +0700

    fix import path for User entity in authStore

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,4 +1,4 @@
-import { type User } from "./../entities/user/user";
+import { type User } from "@/entities/user/user";
 import { defineStore } from "pinia";
 import { jwtDecode } from "jwt-decode";
 import BaseApi from "@/apis/baseApi";
